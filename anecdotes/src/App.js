13d8430657e9f2d411fffab2467e2bd93635dd92
xjyssg <jiayuexue30@gmail.com>
2020-09-29 17:48:00 +0800

    6.3

--- anecdotes/src/App.js
+++ anecdotes/src/App.js
@@ -1,13 +1,10 @@
 import React from 'react'
 import { useSelector, useDispatch } from 'react-redux'
+import {addVote} from './reducers/anecdoteReducer'
 
 const App = () => {
   const anecdotes = useSelector(state => state)
   const dispatch = useDispatch()
-
-  const vote = (id) => {
-    console.log('vote', id)
-  }
 
   return (
     <div>
@@ -19,7 +16,7 @@ const App = () => {
           </div>
           <div>
             has {anecdote.votes}
-            <button onClick={() => vote(anecdote.id)}>vote</button>
+            <button onClick={() => dispatch(addVote(anecdote.id))}>vote</button>
           </div>
         </div>
       )}
--- anecdotes/src/reducers/anecdoteReducer.js
+++ anecdotes/src/reducers/anecdoteReducer.js
@@ -22,8 +22,26 @@ const initialState = anecdotesAtStart.map(asObject)
 const reducer = (state = initialState, action) => {
   console.log('state now: ', state)
   console.log('action', action)
+  switch(action.type) {
+    case 'ADD_VOTE':
+      const id = action.data.id
+      const anecdoteToChange = state.find(a => a.id === id)
+      const changedAnecdote = {
+        ...anecdoteToChange,
+        votes: anecdoteToChange.votes + 1
+      }
+      return state.map(anecdote => 
+        anecdote.id === id ? changedAnecdote : anecdote)
+  }
 
   return state
+}
+
+export const addVote = (id) => {
+  return {
+    type: 'ADD_VOTE',
+    data: {id}
+  }
 }
 
 export default reducer
